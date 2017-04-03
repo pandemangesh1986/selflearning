e8bff18418c7071b41d8e0bae5f3c3e3d042e400
Mangesh <pande.mangesh1986@gmail.com>
2017-04-02 20:57:06 -0500

    Change-Id: I55ebf0c5ae32a5e3bbf2bad28ae0fd5ce6fb14c0

---
+++ TestJavaProg/src/test/FoodfactoryExample.java
@@ -0,0 +1,18 @@
+package test;
+
+public class FoodfactoryExample {
+
+	public static void main(String[] args) {
+		FoodFactory myFoods =new FoodFactory();
+		Food food1 = myFoods.getFood("FastFood");
+		Food food2 = myFoods.getFood("Fruit");
+		System.out.println("My name is: "+food1.getClass().getName());
+		System.out.println("My name is: "+food2.getClass().getName());
+		System.out.println("Our Super class is : " + food1.getClass().getSuperclass().getName());
+		food1.serveFood();
+		food2.serveFood();
+		
+	}
+	
+		
+}
